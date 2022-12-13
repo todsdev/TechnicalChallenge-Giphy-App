cc3d80f66d4308557d94ee1d7cc9c9388106a0fe
Thiago Rodrigues <trod.dev@gmail.com>
2022-12-13 17:38:36 -0300

    Dimens commit

--- app/src/main/java/com/tods/giphy_project/util/Constants.kt
+++ app/src/main/java/com/tods/giphy_project/util/Constants.kt
@@ -2,7 +2,7 @@ package com.tods.giphy_project.util
 
 object Constants {
     const val API_KEY = "ESqnP9hOgncs1T1zUFYHTMYwTOnRNiWv"
-    const val LIMIT = 25
+    const val LIMIT = 250
     const val RATING = "g"
     const val BASE_URL = "https://api.giphy.com/v1/gifs/"
     const val OFF_SET = 0
